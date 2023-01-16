0a4df2171a9506c7ab22a8ad3371fabf1b83bdcd
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-01-16 18:05:24 +0530

    160

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
 
 productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
     var w = document.documentElement.clientWidth || window.innerWidth;
  
     nxtBtn[i].addEventListener('click', () => {
