9e7acf24980f81cd69392e6928bcbf3746808ab1
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-01-20 17:34:09 +0530

    206

--- index.js
+++ index.js
@@ -131,12 +131,13 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let slideimg = document.getElementsByClassName("sliderimg")
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-    
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); 
+  
+  setTimeout(showSlides, 3000); 
 }
