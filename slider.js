6016f8dc830c996c874231d54de3c44d3dabc03d
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-05-17 18:41:59 +0530

    173

---
+++ slider.js
@@ -0,0 +1,65 @@
+var swiper = new Swiper(".mySwiper", {
+// slidesPerView: 2,
+loop: true,
+centeredSlides: false,
+spaceBetween: 0,
+pagination: {
+el: ".swiper-pagination",
+type: "fraction",
+},
+navigation: {
+nextEl: ".swiper-button-next",
+prevEl: ".swiper-button-prev",
+},
+breakpoints: {
+  640: {
+    slidesPerView: 1,
+    spaceBetween: 0,
+  },
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 0,
+  },
+  1024: {
+    slidesPerView: 2,
+    spaceBetween: 0,
+  },
+},
+});
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+.querySelector(".prepend-2-slides")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper.prependSlide([
+  '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+  '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+]);
+});
+document
+.querySelector(".prepend-slide")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper.prependSlide(
+  '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+);
+});
+document
+.querySelector(".append-slide")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper.appendSlide(
+  '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+);
+});
+document
+.querySelector(".append-2-slides")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper.appendSlide([
+  '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+  '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+]);
+});
