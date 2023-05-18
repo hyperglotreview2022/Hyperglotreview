fb1311d9c08129e230635d21ddf38a3345a998be
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-05-18 10:54:44 +0530

    181

--- slider.js
+++ slider.js
@@ -27,6 +27,34 @@ breakpoints: {
 },
 });
 
+var swiper2 = new Swiper(".mySwiper2", {
+  // slidesPerView: 2,
+  loop: true,
+  spaceBetween: 0,
+  pagination: {
+  el: ".swiper-pagination",
+  type: "fraction",
+  },
+  navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+  },
+  });
+
 var appendNumber = 4;
 var prependNumber = 1;
 document
@@ -59,6 +87,43 @@ document
 .addEventListener("click", function (e) {
 e.preventDefault();
 swiper.appendSlide([
+  '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+  '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+]);
+});
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+.querySelector(".prepend-2-slides")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper2.prependSlide([
+  '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+  '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+]);
+});
+document
+.querySelector(".prepend-slide")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper2.prependSlide(
+  '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+);
+});
+document
+.querySelector(".append-slide")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper2.appendSlide(
+  '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+);
+});
+document
+.querySelector(".append-2-slides")
+.addEventListener("click", function (e) {
+e.preventDefault();
+swiper2.appendSlide([
   '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
   '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
 ]);
