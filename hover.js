9d0404ec6c8139e56b13d1c8f1646bbb2976ce92
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-01-10 18:59:10 +0530

    101

---
+++ hover.js
@@ -0,0 +1,24 @@
+function changeURL1() {
+  document.querySelector(".img").style.display = "none";
+  document.querySelector(".img1").style.display = "block";
+}
+function changeURLback1() {
+  document.querySelector(".img1").style.display = "none";
+  document.querySelector(".img").style.display = "block";
+}
+function changeURL2() {
+  document.querySelector(".img").style.display = "none";
+  document.querySelector(".img2").style.display = "block";
+}
+function changeURLback2(){
+  document.querySelector(".img2").style.display = "none";
+  document.querySelector(".img").style.display = "block";
+}
+function changeURL3() {
+  document.querySelector(".img").style.display = "none";
+  document.querySelector(".img3").style.display = "block";
+}
+function changeURLback3(){
+  document.querySelector(".img3").style.display = "none";
+  document.querySelector(".img").style.display = "block";
+}
